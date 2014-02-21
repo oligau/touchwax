fe6d25a955f98cc9b67e904b9692160caf01cf30
Touchwax <touchwax@Dinf-0014-16C.local>
2014-02-21 16:53:58 -0500

    removed touchwax from .gitignore as it was shadowing folders not just binary.

---
+++ touchwax/macosx/touchwax/touchwax/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  touchwax
+//
+//  Created by Touchwax on 2014-02-18.
+//  Copyright (c) 2014 oscille.ca. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
