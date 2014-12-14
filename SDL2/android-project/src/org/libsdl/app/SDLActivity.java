985388647001cd86fbb3c7dadd39d03655f1c2ab
Olivier Gauthier <oligau@oscille.ca>
2014-12-14 15:53:41 -0500

    Added libzip dependency for Android. Addded font for Android.

--- SDL2/android-project/src/org/libsdl/app/SDLActivity.java
+++ SDL2/android-project/src/org/libsdl/app/SDLActivity.java
@@ -45,7 +45,8 @@ public class SDLActivity extends Activity {
         //System.loadLibrary("SDL2_image");
         //System.loadLibrary("SDL2_mixer");
         //System.loadLibrary("SDL2_net");
-        //System.loadLibrary("SDL2_ttf");
+        System.loadLibrary("zip");
+        System.loadLibrary("SDL2_ttf");
         System.loadLibrary("lo");
         System.loadLibrary("main");
     }
@@ -76,6 +77,17 @@ public class SDLActivity extends Activity {
         mLayout.addView(mSurface);
 
         setContentView(mLayout);
+        
+        //// Send C application our package name
+        //PackageInfo info = null;
+        //try {
+           //info = getContext().getPackageManager().getPackageInfo("com.example.text", 0);
+        //} catch( NameNotFoundException e ) {
+           //Log.e( TAG, e.toString() );
+           //return;
+        //}
+        //setAppName( info.applicationInfo.sourceDir );
+        
     }
 
     // Events
